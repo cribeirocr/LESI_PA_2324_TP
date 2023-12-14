10620cb3f8958a23e182c2b02b0b30114469adfe
Carlos Ribeiro <cribeiro.cr@gmail.com>
2023-12-14 23:34:53 +0000

    News_Activity

--- Mobile_App/app/src/main/java/com/example/mobile_app/MainActivity.kt
+++ Mobile_App/app/src/main/java/com/example/mobile_app/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mobile_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        setContentView(R.layout.activity_home)
+
+        val btnAbout = findViewById<Button>(R.id.btn_about)
+        btnAbout.setOnClickListener {
+            val intent = Intent(this, activity_about::class.java)
+            startActivity(intent)
+        }
     }
 }
